5a40c286f58e7286d51ec6b9811e7680149dca76
Keith Booher <keith.booher@yahoo.com>
2018-08-25 18:21:46 -0500

    back to squar one

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(logger("combined"));
 
 // Static
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static("client/build"));
 }
 
 // Routes
